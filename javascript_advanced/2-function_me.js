7c11aabd0f05107f4551a117cd72977d953ab958
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-09-24 22:59:14 +0300

    task 2

---
+++ javascript_advanced/2-function_me.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/* create a function which accepts one argument */
+
+function welcomeMessage(fullName){
+    alert('welcome ' + fullName);
+
+}
+
+let guillaume = welcomeMessage('Guillaume');
+let alex = welcomeMessage('Alex');
+let fred = welcomeMessage('Fred');
