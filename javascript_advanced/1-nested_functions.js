cc3c8ac27e3b2f6d52ada67e2b390d98246c5afc
Nicholas Kipruto <nicholaskipruto112@gmail.com>
2023-09-24 22:03:39 +0300

    task 1

---
+++ javascript_advanced/1-nested_functions.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/* creating functions within other functions */
+let globalVariable = 'welcome ';
+function outer(){
+    alert(globalVariable);
+    let course = 'Holberton';
+    function inner(){
+        alert(globalVariable + course);
+        let exclamation = '!';
+        function inception(){
+            alert(globalVariable + course + exclamation);
+
+        }
+        inception();
+    }
+    inner();
+
+}
+outer();
+
